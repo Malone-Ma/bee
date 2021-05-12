f5c60d9e73e235ed978f70b24f90f2e2bf98e360
Malone <malone@labyun.cn>
2021-05-12 19:37:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,18 @@
 ## $3 = 1 不安装bee-clef
 
 echo -e "let's start the bee programe... \n"
+echo -e "your current bee path is \n"
+
+echo `pwd`
+
+# 开始安装bee和clef包
+dpkg -i `pwd`/bee-clef_0.4.10_amd64.deb
+dpkg -i `pwd`/bee_0.5.3_amd64.deb
+
+chmod +x `pwd`/clef-service
+
+`pwd`/clef-service start
+
+sleep 1m
+
+bee start --verbosity 5 --swap-endpoint https://goerli.infura.io/v3/304ee59b22ca40eb86be1c051c8d79e2 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /var/lib/bee-clef/clef.ipc
