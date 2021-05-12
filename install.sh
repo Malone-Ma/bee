92dcf311ba9763f49f5e79cc42f2da24a1ea55df
Malone <malone@labyun.cn>
2021-05-12 19:52:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ## $1 = 单服务器节点数量,默认ip按照顺序添加
 ## $2 = 硬盘存储空间设置
 ## $3 = 1 不安装bee-clef
+screen_clef_name = $"clef"
+screen_bee_name = $"bee"
 
 echo -e "let's start the bee programe... \n"
 echo -e "your current bee path is \n"
@@ -15,8 +17,12 @@ dpkg -i `pwd`/bee_0.5.3_amd64.deb
 
 chmod +x `pwd`/clef-service
 
-`pwd`/clef-service start
+cmd_clef=$"`pwd`/clef-service start";
+
+cmd_bee=$"bee start --verbosity 5 --swap-endpoint https://goerli.infura.io/v3/304ee59b22ca40eb86be1c051c8d79e2 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /var/lib/bee-clef/clef.ipc"
+
+screen -x -S $screen_clef_name -p 0 -X stuff "$cmd_clef"
 
 sleep 1m
 
-bee start --verbosity 5 --swap-endpoint https://goerli.infura.io/v3/304ee59b22ca40eb86be1c051c8d79e2 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /var/lib/bee-clef/clef.ipc
+screen -x -S $screen_bee_name -p 0 -X stuff "$cmd_bee"
